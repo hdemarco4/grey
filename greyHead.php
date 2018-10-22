fb4f384ca69c03d6e1b99dfe614bb195aaeed5ce
Heather DeMarco <hdemarco@msudenver.edu>
2018-10-22 01:25:23 -0600

    Adjusted leftColumn for etsy logo fit when on smaller screens

--- greyHead.php
+++ greyHead.php
@@ -10,9 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="Grey, Raven, Castle, Wire, Wrapped, Jewelry">
 
-
     <link rel="stylesheet" type="text/css" href="style.css">
-
 
 </head>
 
